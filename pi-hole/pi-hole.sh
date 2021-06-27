992c470ea3bdf8b4d7e8c3599ff907839804cad2
Matt Lewis <53252594+fearandloathinginithell@users.noreply.github.com>
2021-06-27 20:27:30 +1200

    Create pi-hole.sh

---
+++ pi-hole/pi-hole.sh
@@ -0,0 +1,32 @@
+docker pull pihole/pihole
+docker stop pihole
+docker rm pihole
+
+sleep 15
+
+docker run -d \
+    --name pihole \
+    -p 53:53/tcp \
+    -p 53:53/udp \
+    -p 80:80 \
+    -p 443:443 \
+    -v "/vol2/docker/pihole/etc-pihole/:/etc/pihole/" \
+    -v "/vol2/docker/pihole/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
+    --dns=<INTERNAL_ROUTER> --dns=1.1.1.1 \
+    --restart=unless-stopped \
+    --hostname pi.hole \
+    --network trusted-net \
+    -e TZ="Pacific/Auckland" \
+    -e VIRTUAL_HOST="pi.hole" \
+    -e PROXY_LOCATION="pi.hole" \
+    -e ServerIP="<IP_ADDRESS>" \
+    -e DNS1="<INTERNAL_ROUTER>" \
+    -e DNS2="<DNS_PROVIDER>" \
+    -e DNS_FQDN_REQUIRED="true" \
+    -e CONDITIONAL_FORWARDING="true" \
+    -e CONDITIONAL_FORWARDING_IP="<INTERNAL_ROUTER>" \
+    -e CONDITIONAL_FORWARDING_DOMAIN="tld.co.nz" \
+    -e DNSMASQ_LISTENING="all" \
+    pihole/pihole:latest
+
+sleep 60
